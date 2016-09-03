80e270cc945f708df9a54f3cd8ba44d4d4d6f70f
Patrick Rauscher <prauscher@prauscher.de>
2016-09-03 16:29:35 +0200

    add support for requests-sessions

--- jodel.py
+++ jodel.py
@@ -14,7 +14,7 @@ import base64
 
 
 class Jodel:
-    def __init__(self, uid, location, requests_kwargs={}):
+    def __init__(self, uid, location, session=None):
         self.api_url = "https://api.go-tellm.com/api/v2"
         self.client_id = "81e8a76e-1e02-4d17-9ba0-8a7020261b26"
         self.client_version = "4.14.1"
@@ -23,7 +23,9 @@ class Jodel:
 
         self.uid = uid
         self.location = location
-        self.requests_kwargs = requests_kwargs
+        if session is None
+            session = requests.session()
+        self.session = session
 
         self.token = None
         self.token_expire = None
@@ -44,7 +46,7 @@ class Jodel:
         if auth:
             headers["Authorization"] = "Bearer " + auth_token
 
-        reply = requests.request(method, url, data=body, headers=headers, **self.requests_kwargs)
+        reply = self.session.request(method, url, data=body, headers=headers)
         try:
             return reply.json()
         except Exception as e:
